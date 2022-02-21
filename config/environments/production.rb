dcaeb6fee5eff0c8e3e9ac21b643e6c4402d15b5
unknown <dortizchaves@outlook.com>
2022-02-20 23:21:24 -0600

    google login has been configured with user, but is currently a lobotomite

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  ENV['GOOGLE_OAUTH_CLIENT_ID']='157983403743-rpi7b5i0sjeurl5qvhu10g03fpknof59.apps.googleusercontent.com'
+  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get '/', to: 'users#index'
+  root to: 'dashboards#show'
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    devise_scope :user do
+      get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
+      get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
 
   resources :home
   resources :poin_events
