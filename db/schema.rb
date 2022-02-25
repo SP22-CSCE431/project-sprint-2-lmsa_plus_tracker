1db151a3c6ccfb2a7b3bee1029b17bd02b416b47
unknown <zli2018@tamu.edu>
2022-02-25 13:21:37 -0600

    remove broken signup controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_005542) do
+ActiveRecord::Schema.define(version: 2022_02_25_190843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,9 @@ ActiveRecord::Schema.define(version: 2022_02_23_005542) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
     t.string "avatar_url"
+    t.string "password"
+    t.boolean "ismember"
+    t.datetime "birthdate"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
