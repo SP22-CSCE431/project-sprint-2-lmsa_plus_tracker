934a6047d56405db811e98d467978fccee228ce5
Nikita Udoenko <nudodenko99>
2022-03-01 10:28:38 -0600

    Calendar is now visible

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,31 +2,38 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require moment 
-//= require fullcalendar
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import { Calendar} from "fullCalendar"
 import "channels"
 
-import "bootstrap/dist/js/bootstrap"
-import "bootstrap/dist/css/bootstrap"
+require("@popperjs/core")
+import "bootstrap"
+require("../stylesheets/application")
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import { Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
-function eventCalendar() {
-    return $('.calendar').fullCalendar({ });
-};
-function clearCalendar() {
-    $('.calendar').fullCalendar('delete'); 
-    $('.calendar').html('');
-};
-$(document).on('turbolinks:load', function(){
-    eventCalendar();  
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+document.addEventListener('turbolinks:load', function() {
+    var calendarEl = document.getElementById('calendar');
+  
+    var calendar = new Calendar(calendarEl, {
+      
+      plugins: [ dayGridPlugin ],
+  
+    //   eventSources: [
+    //     {        
+    //       url: "/users/<%= @user.id %>/jobs.json",
+    //     }
+    //   ]
+  
+    });
+  
+    calendar.render();
+  
 });
-$(document).on('turbolinks:before-cache', clearCalendar);
