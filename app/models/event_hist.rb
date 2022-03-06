d7410063e9f303e250cfaf4ebe43b59a2d16fffe
Nikita Udoenko <nudodenko99>
2022-03-06 00:56:44 -0600

    sign in added

--- app/models/event_hist.rb
+++ app/models/event_hist.rb
@@ -1,6 +1,6 @@
 class EventHist < ApplicationRecord
   validates :event_id, presence: true
   validates :user_id, presence: true
-  #validates :sign_in, presence: true
+  validates :sign_in, presence: true
   validates :point_recv, presence: true
 end
